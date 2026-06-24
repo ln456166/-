06aa3b841eb875eafad6272673641ef4db76bce3
ln456166 <ln456166>
2026-06-24 06:51:47 +0000

    fix: 爱奇艺解析器增强，PC+移动端双解析，过滤网站默认标题

--- parsers/IqiyiParser.php
+++ parsers/IqiyiParser.php
@@ -15,14 +15,40 @@ class IqiyiParser extends BaseParser
         $mobileUrl = $url;
         if (strpos($host, 'www.iqiyi.com') !== false) {
             $mobileUrl = str_replace('www.iqiyi.com', 'm.iqiyi.com', $url);
+        } elseif (strpos($host, 'm.iqiyi.com') === false && strpos($host, 'iqiyi.com') !== false) {
+            $mobileUrl = str_replace('iqiyi.com', 'm.iqiyi.com', $url);
         }
 
-        $html = $this->httpGetMobile($mobileUrl);
-        if ($html) {
-            $name = $this->extractNameFromMeta($html);
-            if (empty($name)) $name = $this->extractNameFromTitle($html);
-            if (empty($name)) $name = $this->extractNameFromJsonLd($html);
-            $episode = $this->extractEpisodeFromHtml($html, $episode);
+        $mobileHtml = $this->httpGetMobile($mobileUrl);
+
+        $pcHtml = '';
+        if (strpos($host, 'm.iqiyi.com') === false) {
+            $pcHtml = $this->httpGet($url);
+        }
+
+        if ($mobileHtml) {
+            $name = $this->extractNameFromMeta($mobileHtml);
+            if (empty($name)) $name = $this->extractNameFromTitle($mobileHtml);
+            if (empty($name)) $name = $this->extractNameFromJsonLd($mobileHtml);
+            $episode = $this->extractEpisodeFromHtml($mobileHtml, $episode);
+        }
+
+        if (empty($name) || $this->isDefaultSiteTitle($name)) {
+            if ($pcHtml) {
+                $pcName = $this->extractNameFromMeta($pcHtml);
+                if (empty($pcName)) $pcName = $this->extractNameFromTitle($pcHtml);
+                if (empty($pcName)) $pcName = $this->extractNameFromJsonLd($pcHtml);
+                $pcEpisode = $this->extractEpisodeFromHtml($pcHtml, $episode);
+
+                if (!empty($pcName) && !$this->isDefaultSiteTitle($pcName)) {
+                    $name = $pcName;
+                    if ($pcEpisode > 0) $episode = $pcEpisode;
+                }
+            }
+        }
+
+        if (empty($name) || $this->isDefaultSiteTitle($name)) {
+            $name = $this->extractNameFromUrl($url);
         }
 
         if (!empty($name)) {
@@ -30,6 +56,30 @@ class IqiyiParser extends BaseParser
             return ['name' => $name, 'episode' => $episode];
         }
 
-        return $this->parseByHtml($url);
+        return false;
+    }
+
+    private function isDefaultSiteTitle($title)
+    {
+        $defaultTitles = [
+            '爱奇艺-在线视频网站',
+            '海量正版高清视频在线观看',
+            '爱奇艺',
+            'iqiyi',
+        ];
+        foreach ($defaultTitles as $default) {
+            if (mb_strpos($title, $default) !== false && mb_strlen($title, 'UTF-8') < 30) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private function extractNameFromUrl($url)
+    {
+        if (preg_match('/m\.iqiyi\.com\/v_([a-zA-Z0-9]+)/', $url, $m)) {
+            return '';
+        }
+        return '';
     }
 }
